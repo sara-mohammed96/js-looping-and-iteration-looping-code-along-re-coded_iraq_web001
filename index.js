d525747cef367b3be6dd7abc3fa86364626026e1
sara altaee <sara.korea96@gmail.com>
2020-05-14 21:55:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(name , event){
+  let arr=[];
+  for(let i=0;i<name.length;i++)
+  {arr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);}
+  return arr;
+  
+}
+
+
+writeCards(["Lisa", "Kaitlin", "Jan"], "birthday");
+
+function countDown(num){
+ while(num>=0){
+    console.log(num);
+    num--;
+    
+  } 
+}
+countDown(10);
