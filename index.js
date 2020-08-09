1d4432e42baf81e05b1a21f5ce3b397059a2b37e
sara altaee <sara.korea96@gmail.com>
2020-08-09 20:45:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // Code your solutions in this file
 
 function writeCards(name , event){
+<<<<<<< HEAD
   let arr=[];
   for(let i=0;i<name.length;i++)
   {arr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);}
   return arr;
+=======
+  for(let i=0;i<name.length;i++)
+  console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+>>>>>>> 890de83a94f7527b817e07be14c3d0ca22f66340
   
 }
 
 
+<<<<<<< HEAD
 writeCards(["Lisa", "Kaitlin", "Jan"], "birthday");
+=======
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+>>>>>>> 890de83a94f7527b817e07be14c3d0ca22f66340
 
 function countDown(num){
  while(num>=0){
